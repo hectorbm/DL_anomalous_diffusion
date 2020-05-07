eee2cd48690c5b21a4c84d36ec34f5572f057186
Hector Buena Maizon <buenamaizonhector@hotmail.com>
2020-05-07 00:32:16 -0300

    evaluation method and validation Hurst Exp net added

--- networks/hurst_exp_network_model.py
+++ networks/hurst_exp_network_model.py
@@ -1,11 +1,14 @@
 from mongoengine import StringField
-
+import numpy as np
 from networks.generators import generator_hurst_exp_network
 from networks.network_model import NetworkModel
 from keras.layers import Dense, Input, LSTM
 from keras.models import Model
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from keras.optimizers import Adam
+from sklearn.metrics import mean_squared_error
+from physical_models.models_fbm import FBM
+from tracks.simulated_tracks import SimulatedTrack
 
 
 class HurstExponentNetworkModel(NetworkModel):
@@ -53,7 +56,36 @@ class HurstExponentNetworkModel(NetworkModel):
         self.keras_model = hurst_exp_keras_model
 
     def evaluate_track_input(self, track):
-        pass
+        assert (track.track_length == self.track_length), "Invalid track length"
+        prediction = np.zeros(shape=track.n_axes)
+        out = np.zeros(shape=(1, 2, self.track_length))
+
+        for i in range(track.n_axes):
+            zero_mean_x = track.axes_data[str(i)] - np.mean(track.axes_data[str(i)])
+            zero_mean_x = zero_mean_x / np.std(zero_mean_x)
+            out[0, 0, :] = zero_mean_x
+            out[0, 1, :] = np.linspace(0, 1, self.track_length)
+            prediction[i] = self.keras_model.predict(out)[0, 0]
+
+        return np.mean(prediction)
 
     def validate_test_data_mse(self, n_axes):
-        pass
+        test_batch_size = 100
+        mse = np.zeros(shape=test_batch_size)
+
+        for i in range(test_batch_size):
+            if self.fbm_type == "subdiffusive":
+                model_sample = FBM.create_random_subdiffusive()
+            else:
+                model_sample = FBM.create_random_superdiffusive()
+
+            x_noisy, y_noisy, x, y, t = model_sample.simulate_track(track_length=self.track_length,
+                                                                    track_time=self.track_time)
+            ground_truth = model_sample.hurst_exp
+            noisy_data = noisy_data = [x_noisy, y_noisy]
+            track = SimulatedTrack(track_length=self.track_length, track_time=self.track_time,
+                                   n_axes=n_axes, model_type=model_sample.__class__.__name__)
+            track.set_axes_data(axes_data=noisy_data)
+            track.set_time_axis(time_axis_data=t)
+            prediction = self.evaluate_track_input(track)
+            mse[i] = mean_squared_error(ground_truth, prediction)
