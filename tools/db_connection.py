7e3a276d5aaa50b78cd8c06bbe59e7d28ccc0c9f
hector buena maizon <buenamaizonhector@hotmail.com>
2020-10-29 11:57:03 -0300

    TypeError fixed

--- tools/db_connection.py
+++ tools/db_connection.py
@@ -1,9 +1,13 @@
 from mongoengine import connect
 from mongoengine import disconnect
 
+db_name = 'tracking'
+db_ip = '127.0.0.1'
+db_port = 27017
+
 
 def connect_to_db():
-    connect(db='tracking', host='127.0.0.1', port=27017)
+    connect(db=db_name, host=db_ip, port=db_port)
 
 
 def disconnect_to_db():
--- worker_config.py
+++ worker_config.py
@@ -6,8 +6,8 @@ num_workers = 1
 worker_mode = False
 env_vars_error = True
 try:
-    worker_id = os.environ['WORKER_ID']
-    num_workers = os.environ['NUM_WORKERS']
+    worker_id = int(os.environ['WORKER_ID'])
+    num_workers = int(os.environ['NUM_WORKERS'])
     env_vars_error = False
 except KeyError:
     print('Please set OS environment variables for worker mode')
